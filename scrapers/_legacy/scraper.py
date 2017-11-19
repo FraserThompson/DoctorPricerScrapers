2660928fce9c539c889f63d332d14ddccb4f5508
Fraser Thompson <fraserthompson0@gmail.com>
2017-11-19 13:31:26 +1300

    Add a legacy import to get all the old stuff in

---
+++ scrapers/_legacy/scraper.py
@@ -0,0 +1,21 @@
+import csv, json
+import os, sys, codecs
+from scrapers import common as scrapers
+current_dir = './scrapers/_legacy/'
+
+def scrape(name):
+
+	scraper = scrapers.Scraper(name)
+
+	with open(current_dir + 'data.json', 'r') as inFile:
+
+		prac_dict = json.load(inFile)
+
+		for practice in prac_dict:
+			if practice['prices'] and practice['lat']:
+				print(practice['name'])
+				scraper.newPractice(practice['name'], practice['url'], practice['pho'], practice['restriction'])
+				scraper.practice = practice
+				scraper.finishPractice()
+
+	return scraper.finish()
