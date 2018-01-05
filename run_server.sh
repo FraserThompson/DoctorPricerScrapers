2b230e6ee96717eb9517d9680f67062c7e0ddea7
Fraser Thompson <fraserthompson0@gmail.com>
2018-01-05 17:34:19 +1300

    Extend timeout

--- run_server.sh
+++ run_server.sh
@@ -5,4 +5,4 @@ sleep 10
 /usr/local/bin/migrate
 /usr/local/bin/createsuperuser
 python manage.py collectstatic --no-input
-gunicorn --timeout 120 --bind=unix:/run/django/gunicorn.sock -w 1 --reload wsgi
+gunicorn --timeout 300 --bind=unix:/run/django/gunicorn.sock -w 1 --reload wsgi
