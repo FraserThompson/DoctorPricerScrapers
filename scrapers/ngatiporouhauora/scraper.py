cb7360237974fe98ca723d7bee7d352f119c3b3e
Fraser Thompson <fraserthompson0@gmail.com>
2018-01-09 16:59:21 +1300

    Fiux ngati porou hauora

--- scrapers/ngatiporouhauora/scraper.py
+++ scrapers/ngatiporouhauora/scraper.py
@@ -4,26 +4,24 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
 import re
 
-current_dir = './nph/'
-
-def scrape():
+def scrape(name):
+	scraper = scrapers.Scraper(name)
 	url = 'http://www.nph.org.nz/our-clinics/'
 	listUrlSouped = scrapers.openAndSoup(url)
 
-	practices_list = []
-	error_list = []
-	warning_list = []
-
 	prac_rows = listUrlSouped.find('div', {'class', 'grid_4'}).find_all('a', href=True)
+
 	for row in prac_rows:
-		prices = []
 		name = row.get_text(strip=True)
 		url = 'http://www.nph.org.nz' + row['href']
+		scraper.newPractice(name, url, 'Ngati Porou Hauora', "")
+
+		scraper.practice['prices'] = []
 		prac_page = scrapers.openAndSoup(url)
 		contact_box = prac_page.find_all('div', {'class', 'grid_4'})[1].find_all('p')
 
 		if name == "Puhi Kaiti Medical Centre":
-			prices = [
+			scraper.practice['prices'] = [
 	            {
 	                "age": 0,
 	                "price": 0.0
@@ -39,45 +37,20 @@ def scrape():
 			]
 
 			contact = contact_box[1].get_text().split('Ph: ')
-			address = contact[0]
-			phone = contact[1]
+			scraper.practice['address'] = contact[0]
+			scraper.practice['phone'] = contact[1]
 		elif name == "Ruatoria Community Health Centre":
 			contact = contact_box[0].get_text().split('PH: ')
-			address = contact[0]
-			phone = contact[1]
+			scraper.practice['address'] = contact[0]
+			scraper.practice['phone'] = contact[1]
 		else:
 			address_p = contact_box[0].contents
-			address = ""
+			scraper.practice['address'] = ""
 			for thing in address_p:
 				if isinstance(thing, str):
-					address += thing + " "
-			phone = re.split('Ph: |PH: |Phone', contact_box[1].get_text(strip=True))[1]
+					scraper.practice['address'] += thing + " "
+			scraper.practice['phone'] = re.split('Ph: |PH: |Phone', contact_box[1].get_text(strip=True))[1]
 		
-		coord = scrapers.geolocate(address)
+		scraper.finishPractice()
 
-		if (coord[0] == 0):
-			error_list.append(name + ": Cannot geolocate address: " + address)
-			continue
-
-
-
-		# Make the dictionary object
-		practice = {
-			'name': name,
-			'url': url,
-			'address': address,
-			'phone': phone,
-			'restriction': '',
-			'pho': 'Ngati Porou Hauora',
-			'lat': coord[0],
-			'lng': coord[1],
-			'prices': prices
-		}
-		
-		scrapers.postToDatabase(practice, warning_list);
-		practices_list.append(practice)
-
-	with io.open(current_dir + 'data.json', 'w', encoding='utf8') as outFile:
-		json.dump(practices_list, outFile, ensure_ascii=False, sort_keys=True, indent=4)
-
-	scrapers.dealWithFailure(error_list, warning_list, current_dir)
+	return scraper.finish()
