f04a1922ef6eaa0292bcd27191d208346b2eb585
Fraser Thompson <fraserthompson0@gmail.com>
2018-01-12 16:28:27 +1300

    Fix whanganui regional pho

--- scrapers/whanganuiregionalpho/scraper.py
+++ scrapers/whanganuiregionalpho/scraper.py
@@ -5,147 +5,33 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
 import re
 
-def scrape():
-	url = 'http://www.wrpho.org.nz/resources.html#fee_schedule'
-	listUrlSouped = scrapers.openAndSoup(url)
+def scrape(name):
+	scraper = scrapers.Scraper(name)
+	practice_table = scrapers.openAndSoup('http://www.wrpho.org.nz/practices').find('div', {'class': 'view-enrolling'}).find('tbody').find_all('tr')
 
-	practices_list = []
-	error_list = []
-	warning_list = []
-	current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-	tables = listUrlSouped.find_all('table')
-	fees_table = tables[2].find_all('tr')
-	vlca_table = tables[3].find_all('tr')
-
-	# Standard practices
-	for row in fees_table[1:]:
+	for row in practice_table:
 		cells = row.find_all('td')
+
 		name = cells[0].get_text(strip=True)
-		practice = scrapers.getDetailsFromSearch(name)
-		if practice == 0:
-			error_list.append(name + ": No details.")
-			continue
+		url = 'http://www.wrpho.org.nz' + cells[0].find('a').attrs['href']
 
-		if name == "Dr Moore":
-			prices = [
-				{
-					'age': 0,
-					'price': 0
-				},
-				{
-					'age': 6,
-					'price': 25.00
-				},
-				{
-					'age': 13,
-					'price': scrapers.getFirstNumber(cells[2].get_text(strip=True))
-				},
-				{
-					'age': 25,
-					'price': scrapers.getFirstNumber(cells[4].get_text(strip=True))
-				},
-				{
-					'age': 45,
-					'price': scrapers.getFirstNumber(cells[5].get_text(strip=True))
-				},
-				{
-					'age': 65,
-					'price': scrapers.getFirstNumber(cells[6].get_text(strip=True))
-				}
-			]
-		else:	
-			prices = [
-				{
-					'age': 0,
-					'price': 0
-				},
-				{
-					'age': 13,
-					'price': scrapers.getFirstNumber(cells[2].get_text(strip=True))
-				},
-				{
-					'age': 25,
-					'price': scrapers.getFirstNumber(cells[4].get_text(strip=True))
-				},
-				{
-					'age': 45,
-					'price': scrapers.getFirstNumber(cells[5].get_text(strip=True))
-				},
-				{
-					'age': 65,
-					'price': scrapers.getFirstNumber(cells[6].get_text(strip=True))
-				}
-			]
-		
-		if len(prices) == 0:
-			warning_list.append(name + ": No prices.")
+		scraper.newPractice(name, url, "Whanganui Regional PHO", "")
 
-		# Make the dictionary object
-		practice['name'] = name
-		practice['restriction'] = ''
-		practice['pho'] = 'Whanganui Regional PHO'
-		practice['prices'] = prices
-		
-		scrapers.postToDatabase(practice, warning_list);
-		practices_list.append(practice)
+		practice_page = scrapers.openAndSoup(url)
 
-	#VLCA practices LOL IM COPYPASTING CODE
-	for row in vlca_table[1:]:
-		cells = row.find_all('td')
-		name = cells[0].get_text(strip=True)
-		practice = scrapers.getDetailsFromSearch(name)
-		if practice == 0:
-			error_list.append(name + ": No details.")
-			continue
+		scraper.practice['address'] = practice_page.find('div', {'class': 'field-name-field-street-address'}).find('div', {'class': 'field-items'}).get_text(strip=True)
+		scraper.practice['phone'] = practice_page.find('div', {'class': 'field-name-field-phone'}).find('div', {'class': 'field-items'}).get_text(strip=True)
 
-		if name == "Te Oranganui Medical Centres":
-			prices = [
-				{
-					'age': 0,
-					'price': 0
-				},
-				{
-					'age': 13,
-					'price': scrapers.getFirstNumber(cells[2].get_text(strip=True))
-				},
-				{
-					'age': 18,
-					'price': 17.00
-				},
-				{
-					'age': 65,
-					'price': 12.00
-				}
-			]
-		else:
-			prices = [
-				{
-					'age': 0,
-					'price': 0
-				},
-				{
-					'age': 13,
-					'price': scrapers.getFirstNumber(cells[2].get_text(strip=True))
-				},
-				{
-					'age': 18,
-					'price': scrapers.getFirstNumber(cells[3].get_text(strip=True))
-				}
-			]
+		fees_table = practice_page.find('div', {'class': 'field-name-field-consult-fees'}).find_all('td')
 
-		if len(prices) == 0:
-			warning_list.append(name + ": No prices.")
+		for cell in fees_table:
+			split = list(cell.stripped_strings)
 
-		# Make the dictionary object
-		practice['name'] = name
-		practice['restriction'] = ''
-		practice['pho'] = 'Whanganui Regional PHO (VLCA)'
-		practice['prices'] = prices
-		
-		scrapers.postToDatabase(practice, warning_list);
-		practices_list.append(practice)
+			age = scrapers.getFirstNumber(split[0])
+			price = scrapers.getFirstNumber(split[1])
 
-	with io.open(current_dir + '\\data.json', 'w', encoding='utf8') as outFile:
-		json.dump(practices_list, outFile, ensure_ascii=False, sort_keys=True, indent=4)
+			scraper.practice['prices'].append({'age': age, 'price': price})
 
-	scrapers.dealWithFailure(error_list, warning_list, current_dir)
+		scraper.finishPractice()
+
+	return scraper.finish()
