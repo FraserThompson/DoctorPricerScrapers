576232bd3848be2102c5c99e3a0c125dfb627c2b
Fraser Thompson <fraserthompson0@gmail.com>
2018-08-04 19:13:37 +1200

    Fix this

--- dp_server/admin.py
+++ dp_server/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from dp_server import models
 
-admin.site.register(models.Practice)
 admin.site.register(models.Prices)
 admin.site.register(models.Logs)
-admin.site.register(models.Pho)
 
 @admin.register(models.Practice)
 class PracticeAdmin(admin.ModelAdmin):
