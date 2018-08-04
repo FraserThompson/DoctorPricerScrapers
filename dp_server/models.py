b5e7fdd847a18d3c7ecc0dd1a3f1fbc7a15ff4fc
Fraser Thompson <fraserthompson0@gmail.com>
2018-08-04 15:18:52 +1200

    Allow null last scrape

--- dp_server/models.py
+++ dp_server/models.py
@@ -21,8 +21,8 @@ class Pho(models.Model):
     last_run = models.DateTimeField(auto_now=True)
     current_task_id = models.TextField(blank=True, null=True, default=None)
     number_of_practices = models.IntegerField(default=0)
-    average_prices = JSONField(blank=True, null=True, default=None)
-    last_scrape = JSONField(blank=True, default=[])
+    average_prices = JSONField(blank=True, null=True, default={})
+    last_scrape = JSONField(blank=True, null=True, default=[])
     history = HistoricalRecords()
 
     def __str__(self):
