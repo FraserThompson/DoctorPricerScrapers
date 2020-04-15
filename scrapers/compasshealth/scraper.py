5339dd1cce54504b48052cb45e22d162ef23fd58
Fraser Thompson <fraserthompson0@gmail.com>
2020-04-15 15:49:45 +1200

    Fix indentation

--- scrapers/compasshealth/scraper.py
+++ scrapers/compasshealth/scraper.py
@@ -10,20 +10,21 @@ current_dir = './scrapers/compasshealth/'
 
 def scrape(name):
 
-	scraper = scrapers.Scraper(name)
+    scraper = scrapers.Scraper(name)
 
     with open(current_dir + 'data.json', 'r') as inFile:
 
         prac_dict = json.load(inFile)
 
         for practiceObj in prac_dict['data']['scraped']:
-            
+
             practice = practiceObj['practice'] if 'practice' in practiceObj else practiceObj
 
             if 'prices' in practice and practice['prices'] and 'lat' in practice and practice['lat']:
                 print(practice['name'])
-                scraper.newPractice(practice['name'], practice['url'], practice['pho'], practice['restriction'])
+                scraper.newPractice(
+                    practice['name'], practice['url'], practice['pho'], practice['restriction'])
                 scraper.practice = practice
                 scraper.finishPractice()
 
-	return scraper.finish()
+        return scraper.finish()
