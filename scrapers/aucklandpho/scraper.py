cb45d08bcf6298e8baf133b6326d67281272f411
Fraser Thompson <fraserthompson0@gmail.com>
2020-04-18 15:15:49 +1200

    Fix two scrapers

--- scrapers/aucklandpho/scraper.py
+++ scrapers/aucklandpho/scraper.py
@@ -5,53 +5,52 @@ import re
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
 
-details_dict = {}
-
-def normalize(input):
-	return scrapers.normalize(input).replace(' st', 'street')
-
 def scrape(name):
 	scraper = scrapers.Scraper(name)
 
-	# Access the URL
-	rootURL = 'http://www.aucklandpho.co.nz'
-	listUrlSouped = scrapers.openAndSoup(rootURL + '/fees/')
-	rows = listUrlSouped.find('table', {'id': 'feestable'}).find_all('tr')[1:]
+	rootURL = 'https://aucklandpho.co.nz'
 
-	pracURLSouped = scrapers.openAndSoup(rootURL + '/practices/practice-locations/')
-	coord_list = pracURLSouped.find_all('div', {'class': 'cggm_marker'})
-	for item in coord_list:
-		comments = item.find_all(text=lambda text:isinstance(text, Comment))
-		comments2 = [comment.extract() for comment in comments]
-		item_phone = comments2[0].split('</b>')[1].split('<br />')[0]
-		item_coord = [float(item.find('input', {'name': 'latitude'}).get('value')), float(item.find('input', {'name': 'longitude'}).get('value'))]
-		item_name = item.find('input', {'name': 'title'}).get('value')
-		item_info = item.find('div', {'class': 'cggm_infowindow_item'})
-		item_address = item_info.get_text().splitlines()[1].split(':')[1].strip()
-		item_url = rootURL + "/" + item_info.find('a').get('href')
-		details_dict.update({normalize(item_name): [item_address, item_url, item_phone, item_coord]})
+	# build a dict with practice info keyed to practice name
+	listUrlSouped = scrapers.openAndSoup(rootURL + '/practices/')
+	practiceEls = listUrlSouped.find('div', {'class': 'practices-fees-items'}).find_all('p')
 
-	print("Done. Iterating rows...")
-	for row in rows:
-		coord = [0,0]
+	practiceInfo = {}
+	for el in practiceEls:
+		
+		linkEl = el.find('a')
+		if not linkEl:
+			break
+
+		name = linkEl.get_text(strip=True)
+		practiceInfo[name] = {}
+		practiceInfo[name]['name'] = name
+		practiceInfo[name]['url'] = linkEl.get('href') if rootURL in linkEl.get('href') else rootURL + linkEl.get('href')
+		practiceInfo[name]['address'] = el.get_text(strip=True).split("Address:")[1]
+		practiceUrlSouped = scrapers.openAndSoup(practiceInfo[name]['url'])
+
+		try:
+			practiceInfo[name]['phone'] = practiceUrlSouped.find('h4', {'class': 'vc_custom_heading'}).get_text(strip=True).split("phone")[1]
+		except IndexError:
+			practiceInfo[name]['phone'] = None
+
+	print("Done. Iterating fee rows...")
+
+	feeEls = listUrlSouped.find('table').find_all('tr')[3:]
+
+	for row in feeEls:
 		cells = row.find_all('td')
-		scraper.newPractice(cells[0].get_text(strip=True), "None", "Auckland PHO", "")
+		name = cells[0].get_text(strip=True)
 
-		normal_name = normalize(scraper.practice['name']).split()
-		scraper.practice['address'] = "None"
-		scraper.practice['phone'] = "None"
-
-		prac_details = [v for k,v in details_dict.items() if k.startswith(' '.join(normal_name[:2]))]
-		if len(prac_details) == 0 or len(prac_details[0]) < 4:
+		try:
+			info = practiceInfo[name]
+		except KeyError:
 			scraper.addError("Could not find details.")
 			continue
-		else:
-			scraper.practice['address'] = prac_details[0][0]
-			scraper.practice['url'] = prac_details[0][1]
-			scraper.practice['phone'] = prac_details[0][2]
-			coord = prac_details[0][3]
 
-		scraper.setLatLng(coord)
+		scraper.newPractice(name, info['url'], "Auckland PHO", "")
+
+		scraper.practice['address'] = info['address']
+		scraper.practice['phone'] = info['phone']
 
 		scraper.practice['prices'] =  [
 				{
@@ -59,24 +58,24 @@ def scrape(name):
 				'price': float(cells[1].get_text(strip=True).replace(" ", "").replace("$", ""))
 				},
 				{
-				'age': 13,
-				'price': float(cells[2].get_text(strip=True).replace(" ", "").replace("$", ""))
-				},
-				{
-				'age': 18,
-				'price': float(cells[3].get_text(strip=True).replace(" ", "").replace("$", ""))
-				},
-				{
-				'age': 25,
+				'age': 14,
 				'price': float(cells[4].get_text(strip=True).replace(" ", "").replace("$", ""))
 				},
 				{
-				'age': 45,
+				'age': 18,
 				'price': float(cells[5].get_text(strip=True).replace(" ", "").replace("$", ""))
 				},
 				{
-				'age': 65,
+				'age': 25,
 				'price': float(cells[6].get_text(strip=True).replace(" ", "").replace("$", ""))
+				},
+				{
+				'age': 45,
+				'price': float(cells[7].get_text(strip=True).replace(" ", "").replace("$", ""))
+				},
+				{
+				'age': 65,
+				'price': float(cells[8].get_text(strip=True).replace(" ", "").replace("$", ""))
 				}
 			]
 
--- scrapers/easthealthtrust/scraper.py
+++ scrapers/easthealthtrust/scraper.py
@@ -1,4 +1,4 @@
-import sys, codecs, os
+import sys, codecs, os, re
 import json
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
@@ -7,64 +7,40 @@ from scrapers import common as scrapers
 def scrape(name):
 	scraper = scrapers.Scraper(name)
 
-	listUrlSouped = scrapers.openAndSoup('https://www.easthealth.co.nz/practices')
-	rows = listUrlSouped.find_all('a', {'class': 'style-jgd359yclink'})
+	listUrlSouped = scrapers.openAndSoup('https://www.easthealth.co.nz/clinics')
+	rows = listUrlSouped.find_all('div', {'class': 'style-jgd359yc'})
 
 	print("Iterating table...")
 	for row in rows:
+		linkEl = row.find('a')
+		name = linkEl.get_text(strip=True).replace(" >", "")
+		scraper.newPractice(name, linkEl.get('href'), "East Health Trust", "")
 
-		name = row.get_text(strip=True).replace(" >", "")
-		scraper.newPractice(name, row.get('href'), "East Health Trust", "")
+		practiceUrlSouped = scraper.openAndSoup().find('main')
 
-		######## GOING IN DEEP #######
-		practiceUrlSouped = scraper.openAndSoup()
-		could_be_anything = practiceUrlSouped.find_all('div', {'data-packed': 'true'})
+		addressText = practiceUrlSouped.find(text="Physical Address").findParent('div', {'class': 'txtNew'}).getText(strip=True)
+		scraper.practice['address'] = addressText.split("Physical Address")[1]
 
-		for thing in could_be_anything:
-			text = thing.get_text(strip=True)
+		phoneText = practiceUrlSouped.find(text="Phone").findParent('div', {'class': 'txtNew'}).getText(strip=True)
+		scraper.practice['phone'] = phoneText.split("Phone")[1]
 
-			if "fees current" in text:
-				continue
+		try:
+			feesTextEl = practiceUrlSouped.find(text=re.compile("^Fees")).findParent('div', {'class': 'txtNew'})
+		except AttributeError:
+			scraper.addError("Couldn't find fees.")
+			continue
 
-			if "Phone" in text:
-				scraper.practice['phone']= text.split("Phone")[1]
-				continue
-			
-			if "Address" in text:
-				scraper.practice['address'] = text.split("Physical Address")[1]
-				continue
+		ages = feesTextEl.findNext('div').findAll('p')
+		prices = feesTextEl.findNext('div').findNext('div').findAll('p')
 
-			if "$" in text:
-				fees = text.split("$")
-				continue
+		scraper.practice['prices'] = []
 
-		#### Fees ####
-		scraper.practice['prices'] =  [
-			{
-			'age': 0,
-			'price': float(fees[0].replace('FREE', '0')),
-			},
-			{
-			'age': 13,
-			'price': float(fees[1]),
-			},
-			{
-			'age': 18,
-			'price': float(fees[2]),
-			},
-			{
-			'age': 25,
-			'price': float(fees[3]),
-			},
-			{
-			'age': 45,
-			'price': float(fees[4]),
-			},
-			{
-			'age': 65,
-			'price': float(fees[5]),
-			},
-		] 
+		for idx, age in enumerate(ages):
+			fee = {
+				'age': scrapers.getFirstNumber(age.get_text(strip=True)),
+				'price': float(scrapers.getFirstNumber(prices[idx].get_text(strip=True).replace('FREE', '0')))
+			}
+			scraper.practice['prices'].append(fee)
 
 		scraper.finishPractice()
 	return scraper.finish()
