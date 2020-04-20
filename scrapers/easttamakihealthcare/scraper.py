bfc77cf993e1e8dbfc7b240efecc95a97fa6717e
Fraser Thompson <fraserthompson0@gmail.com>
2020-04-20 13:34:46 +1200

    Fix scrapers, remove _manual and make into own things

--- scrapers/easttamakihealthcare/scraper.py
+++ scrapers/easttamakihealthcare/scraper.py
@@ -5,53 +5,39 @@ import requests
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
 
-prices_south_gi = [
-	{
-	'age': 0,
-	'price': 0,
-	},
-	{
-	'age': 18,
-	'price': 10,
-	}
-]
-
-prices_dan = [
-	{
-	'age': 0,
-	'price': 0,
-	},
-	{
-	'age': 18,
-	'price': 15,
-	},
-	{
-	'age': 65,
-	'price': 10,
-	}
-]
-
-prices_exception = ['Dannemora', 'Sandringham', 'Sylvia Park']
-
 def scrape(name):
 	scraper = scrapers.Scraper(name)
-	url = 'http://www.ethc.co.nz/121/all-clinics'
+	rootUrl = 'http://www.ethc.co.nz'
+	listUrlSouped = scrapers.openAndSoup(rootUrl + "/121/all-clinics")
 
-	listUrlSouped = scrapers.openAndSoup(url)
-	rows = listUrlSouped.findAll('div', {'class': ['addressbox', 'addressboxalt']})
+	feesUrl = rootUrl + '/fees'
+	feesUrlSouped = scrapers.openAndSoup(feesUrl)
+	feesBlob = feesUrlSouped.find('input', {'id': 'clinic-fees-data'}).get('value')
+	feesDict = json.loads(feesBlob)
+
+	rows = listUrlSouped.findAll('li', {'class': ['c-clinic-list']})
 
 	for row in rows:
-		name =  row.find('h2').get_text() if "ETHC" in row.find('h2').get_text() else "ETHC " + row.find('h2').get_text()
-		print(name)
+		enrolling = row.find('span', {'class': 'c-find-a-clinic__open-tagline'})
+		if "Open" not in enrolling:
+			scraper.notEnrolling()
+
+		name =  row.find('a').get_text(strip=True)
+		url = rootUrl + row.find('a').get('href')
 		scraper.newPractice(name, url, "East Tamaki Healthcare", "")
 
-		scraper.practice['address'] = row.find('img')['src'].split('center=')[1].split('&')[0]
-		scraper.practice['phone'] = row.find('h3').get_text().split(': ')[1].split('Fax: ')[0]
+		scraper.practice['address'] = row.find('p', {'class': 'u-mb--xs'}).get_text(strip=True)
+		scraper.practice['phone'] = row.find('ul', {'class': 'c-find-a-clinic__contact-list'}).find('span').get_text(strip=True)
 
-		if (scraper.practice['name'] in prices_exception):
-			scraper.practice['prices'] = prices_dan
-		else:
-			scraper.practice['prices'] = prices_south_gi
+		practicePage = scrapers.openAndSoup(url)
+		clinicCode = practicePage.find('input', {'id': 'clinic-fees-code'}).get('value')
+
+		filteredFees = [x for x in feesDict if x['ClinicCode'] == clinicCode and x['ServiceType'] == "Normal Hours Medical Consult" and x['EnrolmentStatus'] == "Casual" and x['CSCCardStatus'] == "No CSC"]
+		scraper.practice['prices'] = []
+		print(filteredFees)
+		for fee in filteredFees:
+			feeObj = {'age': scrapers.getFirstNumber(fee['AgeGroup']), 'price': scrapers.getFirstNumber(fee['Price'])}
+			scraper.practice['prices'].append(feeObj)
 
 		scraper.finishPractice()
 
