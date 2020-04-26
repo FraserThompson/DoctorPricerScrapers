ff236e96a15fa0897b6f83b9f1def93bda3353ab
Fraser Thompson <fraserthompson0@gmail.com>
2020-04-26 13:16:53 +1200

    Fix more scrapers

--- scrapers/tekorowai/scraper.py
+++ scrapers/tekorowai/scraper.py
@@ -7,21 +7,28 @@ from scrapers import common as scrapers
 def scrape(name):
 	scraper = scrapers.Scraper(name)
 
-	feesList = scrapers.openAndSoup('http://www.korowai.co.nz/services/fees').find('table', {'class': 'fees'}).find_all('tr')
-	prices = []
-	for row in feesList[2:6]:
-		cells = row.find_all('td')
-		prices.append({'age': scrapers.getFirstNumber(cells[0].get_text(strip=True).replace("Under 18s", "6")), 'price': float(cells[1].get_text(strip=True).replace("Free", "0").replace("$", ""))})
-
 	practiceListUrlSouped = scrapers.openAndSoup('http://www.korowai.co.nz/contact_us')
 	practiceRoot = practiceListUrlSouped.find('div', {'class': 'page-content'})
 	practiceNames = practiceRoot.find_all('h3')
 
 	for row in practiceNames:
 		scraper.newPractice(row.get_text(strip=True), "http://www.korowai.co.nz/", "Te Korowai", "")
-		scraper.practice['prices'] = prices
 
-		# they changed their website and got rid of useful info so we'll just use what we got for now I guess
+		# These prices come from http://www.korowai.co.nz/services/fees and are the same for all practices
+		scraper.practice['prices'] = [
+			{
+				'age': 0,
+				'price': 0
+			},
+			{
+				'age': 18,
+				'price': 18
+			},
+			{
+				'age': 65,
+				'price': 12
+			}
+		]
 
 		scraper.finishPractice()
 
--- scrapers/westernbayofplentypho/scraper.py
+++ scrapers/westernbayofplentypho/scraper.py
@@ -3,81 +3,83 @@ import json, io
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '//..//')
 from scrapers import common as scrapers
 import re
+import requests
 
 def scrape(name):
 	scraper = scrapers.Scraper(name)
 
 	fees_dict = {}
-	root = 'http://www.wboppho.org.nz/'
+	root = 'https://www.wboppho.org.nz'
+	feesUrl = root + '/fees-and-subsidies/'
 
-	feesUrlSouped = scrapers.openAndSoup(root + 'medical-centres/what-is-primary-health-general-practice-fees/')
-	fees_rows = feesUrlSouped.find('div', {'class': 'col-md-8'}).find('table').find_all('tr')[1:]
+	# These guys have this weird little API so that's cool
+	fees = requests.post(feesUrl, {'get_fees': True}).json()
+	prices = {}
 
-	# Assemble fees dict
-	for row in fees_rows:
-		cells = row.find_all('td')
+	# Assemble fees dict for use when scraping practice details
+	for row in fees:
+		name = row['title']
 
-		if not cells:
+		# Some of them are straight broked
+		if not row['ages_14_17']:
 			continue
 
-		name = cells[0].get_text(strip=True)
-		if name == '':
-			break
-		if name == "Papamoa Pines Health Centre":
-			name = "Papamoa Pines Medical Centre"
-
-		prices = [
-				{
+		fees = [
+			{
 				'age': 0,
-				'price': 0,
-				},
-				{
+				'price': 0
+			},
+			{
 				'age': 14,
-				'price': float(cells[4].get_text(strip=True).replace('$', '')),
-				},
-				{
+				'price': float(row['ages_14_17'])
+			},
+			{
 				'age': 18,
-				'price': float(cells[5].get_text(strip=True).replace('$', '')),
-				},
-				{
+				'price': float(row['ages_18_24'])
+			},
+			{
 				'age': 25,
-				'price': float(cells[6].get_text(strip=True).replace('$', '')),
-				},
-				{
+				'price': float(row['ages_25_44'])
+			},
+			{
 				'age': 45,
-				'price': float(cells[7].get_text(strip=True).replace('$', '')),
-				},
-				{
+				'price': float(row['ages_45_64'])
+			},
+			{
 				'age': 65,
-				'price': float(cells[8].get_text(strip=True).replace('$', '')),
-				}
-			]
+				'price': float(row['ages_64up'])
+			},
+		]
 
-		fees_dict[scrapers.normalize(name)] = prices
+		fees_dict[scrapers.normalize(name)] = fees
 
-	pracsURLSouped = scrapers.openAndSoup(root + 'medical-centres/')
-	pracs_rows = pracsURLSouped.find('div', {'class': 'col-md-8'}).find_all('article', {'class': re.compile('medicalList *')})
+	pracsURLSouped = scrapers.openAndSoup(root + '/medical-centres/')
+	pracs_rows = pracsURLSouped.find('div', {'class': 'medicalCentres'}).find('table').find('tbody').findAll('tr')
 
 	# Scrape practices
 	for row in pracs_rows:
-		scraper.newPractice(row.find('span', {'class': 'practice'}).get_text(strip=True), "http://www.wboppho.org.nz/", "Western Bay of Plenty PHO", "")
+		cells = row.find_all('td')
 
-		if row.find('span', {'class': 'accepting'}).get_text(strip=True) == 'No':
+		name = cells[0].get_text(strip=True)
+
+		try:
+			url = cells[0].find('a').get('href')
+		except AttributeError:
+			url = root
+
+		scraper.newPractice(name, url, "Western Bay of Plenty PHO", "")
+
+		if cells[3].find('img').get('alt') == "NOT ACCEPTING PATIENTS":
 			scraper.notEnrolling()
 
 		try:
-			scraper.practice['url'] = row.find('a').get('href')
-		except AttributeError:
-			scraper.addWarning('Could not get website.')
-
-		try:
-			prices = fees_dict[scrapers.normalize(scraper.practice['name'])]
+			prices = fees_dict[scrapers.normalize(name)]
 		except:
 			scraper.addWarning('Could not get fees.')
 			prices = []
 
-		scraper.practice['address'] = row.find('span', {'class': 'address'}).get_text(strip=True) + " " + row.find('span', {'class': 'suburb'}).get_text(strip=True)
-		scraper.practice['phone'] = row.find('span', {'class': 'phone'}).get_text(strip=True)
+		scraper.practice['address'] = cells[1].get_text(strip=True)
+		scraper.practice['phone'] = cells[2].get_text(strip=True)
 		scraper.practice['prices'] = prices
 
 		scraper.finishPractice()
