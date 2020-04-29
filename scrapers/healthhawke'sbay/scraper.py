b0d703077374bf941e19f125f7f403617a14fd29
Fraser Thompson <fraserthompson0@gmail.com>
2020-04-30 10:43:02 +1200

    Fix scraper

--- scrapers/healthhawke'sbay/scraper.py
+++ scrapers/healthhawke'sbay/scraper.py
@@ -7,11 +7,15 @@ from scrapers import common as scrapers
 # First URL for getting fees
 def scrape(name):
 	scraper = scrapers.Scraper(name)
-	listUrlSouped = scrapers.openAndSoup('http://www.ourhealthhb.nz/health-services/family-doctor-gp-fees-for-enrolled-patients/')
-	fees_table = listUrlSouped.find('table').find_all('tr')
+	rootUrl = 'http://www.ourhealthhb.nz'
+
+	feesUrlSouped = scrapers.openAndSoup(rootUrl + '/your-health/gp-fees-for-enrolled-patients/')
+	fees_table = feesUrlSouped.find('table').find_all('tr')
 
 	ages = []
 	wairoa = False
+
+	allPrices = {}
 
 	for index, row in enumerate(fees_table):
 
@@ -46,58 +50,40 @@ def scrape(name):
 		if len(cells) <= 3 or not name:
 			continue
 
-		url_name = name
-
-		# These practices have URLs which differ from their names (from here http://www.ourhealthhb.nz/health-services/find-a-family-doctor-or-gp/?Locations%5B0%5D=24&Locations%5B1%5D=0&action_doFindService=Go#Form_Form)
-		if name == "Tuki Tuki Medical Ltd":
-			url_name = "tukituki-medical"
-
-		if name == "The Havelock North Health Centre":
-			url_name = "dr-wakefields-practice"
-
-		if name == "Gascoigne Medical Centre":
-			url_name = "gascoigne-medical-services"
-		
-		if name == "Mahora Medical Centre":
-			url_name = "mahora-medical-trust"
-
-		if name == "Hawke’s Bay Wellness Centre":
-			url_name = "hb-wellness-centre"
-
-		if name == "Totara Health – Flaxmere":
-			url_name = "totara-health-limited-flaxmere"
-
-		if name == "Totara Health – Nelson Street":
-			url_name = "totara-health-hastings"
-
-		# These guys work at Maraenui Medical Centre which is already a thing
-		if name == "Dr Paul Hendy" or name == "Dr Robert Harris":
-			continue
-
-		url = 'http://www.ourhealthhb.nz/health-services/find-a-family-doctor-or-gp/' + url_name.lower().replace(' ', '-').replace(',', '').replace('&', 'and').replace('(', '').replace(')', '')
-		scraper.newPractice(name, url, "Health Hawke's Bay", "")
-
-		scraper.practice['prices'] = []
-
+		prices = []
 		# Assign fees to prices
 		for index, age in enumerate(ages):
 	
 			if cells[index + 1].get_text(strip=True):
 				price = float(cells[index + 1].get_text(strip=True).replace('Free', '$0').replace("$", ""))
 
-			scraper.practice['prices'].append({'age': age, 'price': price })
+			prices.append({'age': age, 'price': price })
+		
+		allPrices[scrapers.normalize(name)] = prices
 
-		# Delve deep to find the info
+	listUrlSouped = scrapers.openAndSoup('http://www.ourhealthhb.nz/your-health/find-a-family-doctor-or-gp/')
+	practiceList = listUrlSouped.find('div', {'class': 'rightbar'}).findAll('a')
+
+	for practice in practiceList[1:-2]:
+		name = practice.getText(strip=True)
+		url = rootUrl + practice.get('href')
+		scraper.newPractice(name, url , "Health Hawke's Bay", "")
+
+		if practice.get('class') == 'red':
+			scraper.notEnrolling()
+
 		practice_info = scrapers.openAndSoup(url)
 
 		content = practice_info.find('div', {'class': 'col-md-6 main-content print-center-children'})
 
 		scraper.practice['phone'] = scrapers.better_strip(content.find('h3').stripped_strings).split("Phone ")[1].split("Fax ")[0]
 		scraper.practice['address'] = scrapers.better_strip(content.find_all('p')[0].stripped_strings)
-		enrolling = content.find('img')
+		foundPrices = scrapers.partial_match(scrapers.normalize(name), allPrices)
 
-		if enrolling and enrolling.attrs['src'] == 'assets/Uploads/_resampled/ResizedImageWzE0MCwxNDBd/TLred.jpg':
-			scraper.notEnrolling()
+		if foundPrices == None:
+			scraper.addError('No prices found in fee table (could be name mismatch)')
+		else:
+			scraper.practice['prices'] = foundPrices
 
 		scraper.finishPractice()
 
